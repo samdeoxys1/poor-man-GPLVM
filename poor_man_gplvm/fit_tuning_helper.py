6ac8130ddd8ff679961d79b258d78ae2cb878bac
Sam Zheng <samzhenguchi@gmail.com>
2025-08-07 12:51:32 -0400

    fix

--- poor_man_gplvm/fit_tuning_helper.py
+++ poor_man_gplvm/fit_tuning_helper.py
@@ -91,9 +91,7 @@ def poisson_m_step_objective_smoothness(param,hyperparam,basis_mat,y_weighted,t_
     '''
     param_prior_std = hyperparam['param_prior_std']
     smoothness_penalty = hyperparam['smoothness_penalty'] # if using bspline basis
-    pf_hat = get_tuning_softplus(param,basis_mat) # n_latent x n_neuron
-    # Get tuning curves for each neuron (n_latent x n_neuron)
-    tuning_curves = pf_hat
+    tuning_curves = get_tuning_softplus(param,basis_mat) # n_latent x n_neuron
     
     # Calculate second order finite differences for each neuron's tuning curve
     # Exclude edges since we're not using periodic boundary conditions
@@ -112,8 +110,8 @@ def poisson_m_step_objective_smoothness(param,hyperparam,basis_mat,y_weighted,t_
     roughness_term = smoothness_penalty * total_roughness
     # log_likelihood = jax.scipy.stats.poisson.logpmf(y_weighted,yhat * t_weighted[:,None]).sum()
 
-    norm_term = pf_hat * t_weighted[:,None] # n_latent x n_neuron
-    fit_term = vmap(jscipy.special.xlogy,in_axes=(1,1),out_axes=1)(y_weighted,pf_hat+1e-20) # n_latent x n_neuron
+    norm_term = tuning_curves * t_weighted[:,None] # n_latent x n_neuron
+    fit_term = vmap(jscipy.special.xlogy,in_axes=(1,1),out_axes=1)(y_weighted,tuning_curves+1e-20) # n_latent x n_neuron
     log_likelihood = jnp.sum(fit_term - norm_term) # crucial, this is different from poisson logpmf(s_b_one, pf_one*t_b)!!!!
     log_prior = jax.scipy.stats.norm.logpdf(param,0,param_prior_std).sum()
     return -log_likelihood - log_prior + roughness_term
