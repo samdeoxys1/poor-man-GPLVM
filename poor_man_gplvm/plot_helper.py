01823127f5d75ed9bd472ed8607d3c8ec2f7b599
Sam Zheng <samzhenguchi@gmail.com>
2025-08-12 20:13:39 -0400

    plot_mean_error_plot

--- poor_man_gplvm/plot_helper.py
+++ poor_man_gplvm/plot_helper.py
@@ -19,3 +19,24 @@ def save_fig(fig,fig_name,fig_dir='./figs',fig_format=['png','svg'],dpi=300):
         fig.savefig(os.path.join(fig_dir,fig_name+f'.{fmt}'),dpi=dpi,bbox_inches='tight')
         print(f'saved {fig_name}.{fmt} to {fig_dir}')
     plt.close(fig)
+
+def plot_mean_error_plot(data,error_type='ci',mean_axis=0,fig=None,ax=None,**kwargs):
+    '''
+    plt the mean and error of the data
+    data: pd.DataFrame, 
+    error_type: 'ci' or 'std'
+    mean_axis: axis to take the mean of, same for error; plot the other axis
+    '''
+    if fig is None:
+        fig,ax = plt.subplots()
+    if error_type == 'ci':
+        mean = np.mean(data,axis=mean_axis)
+        error = np.std(data,axis=mean_axis) / np.sqrt(data.shape[mean_axis])
+    elif error_type == 'std':
+        mean = np.mean(data,axis=mean_axis)
+        error = np.std(data,axis=mean_axis)
+    else:
+        raise ValueError(f'error_type {error_type} not supported')
+    ax.plot(mean,**kwargs)
+    ax.fill_between(np.arange(len(mean)),mean-error,mean+error,alpha=0.5)
+    return fig,ax
