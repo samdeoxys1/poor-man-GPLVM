e4454ca8077aefe458e6667d03662fe7e143e782
Sam Zheng <samzhenguchi@gmail.com>
2025-08-12 20:27:52 -0400

    fix names

--- poor_man_gplvm/analysis_helper.py
+++ poor_man_gplvm/analysis_helper.py
@@ -14,14 +14,13 @@ def get_state_interval(p_l,p_thresh=0.8, merge_thresh=1,duration_thresh=2,):
     duration_thresh: threshold for filtering out short intervals
 
     return: 
-    continuous_intv_merge: nap.IntervalSet, the interval of the chunks
+    intv_merge: nap.IntervalSet, the interval of the chunks
     '''
-    p_continuous_thresh = p_thresh # threshold for considering dynamics to be continuous
-    continuous_intv=p_l.threshold(p_continuous_thresh).time_support
-    continuous_intv_merge = continuous_intv.merge_close_intervals(merge_thresh) # threshold for merging adjacent intervals
-    ma=continuous_intv_merge[:,1]-continuous_intv_merge[:,0] > duration_thresh
-    continuous_intv_merge = continuous_intv_merge[ma]
-    return continuous_intv_merge
+    intv=p_l.threshold(p_thresh).time_support
+    intv_merge = intv.merge_close_intervals(merge_thresh) # threshold for merging adjacent intervals
+    ma=intv_merge[:,1]-intv_merge[:,0] > duration_thresh
+    intv_merge = intv_merge[ma]
+    return intv_merge
 
 
 
