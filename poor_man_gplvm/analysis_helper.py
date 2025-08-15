8c474d6c9170763be52727b2284b515519649529
Sam Zheng <samzhenguchi@gmail.com>
2025-08-15 12:53:50 -0400

    dbeug

--- poor_man_gplvm/analysis_helper.py
+++ poor_man_gplvm/analysis_helper.py
@@ -53,7 +53,11 @@ def get_peri_event_with_shuffle(feature_tsd,event_ts,n_shuffle=100,minmax=4,do_z
     if n_shuffle > 0:
         for i in tqdm.trange(n_shuffle):
             event_ts_sh=nap.shift_timestamps(event_ts)
-            event_ts_sh = event_ts_sh[(event_ts_sh.t>minmax) & (event_ts_sh.t<event_ts_sh.t[-1]-minmax)]
+            try:
+                event_ts_sh = event_ts_sh[(event_ts_sh.t>minmax) & (event_ts_sh.t<event_ts_sh.t[-1]-minmax)]
+            except:
+                import pdb; pdb.set_trace()
+
             peri_event_sh=nap.compute_perievent_continuous(timeseries=feature_tsd,tref=event_ts_sh,minmax=minmax) # n_time x n_event
             peri_event_sh = peri_event_sh.as_dataframe().T # n_event x n_time
             if do_zscore:
