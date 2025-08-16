52648044f08b18292272460f8c565afa19ed1404
Sam Zheng <samzhenguchi@gmail.com>
2025-08-16 16:37:07 -0400

    get_decode_res_l_from_em_res_l

--- poor_man_gplvm/post_fit_workflow/post_fit_ach_dataset.py
+++ poor_man_gplvm/post_fit_workflow/post_fit_ach_dataset.py
@@ -19,3 +19,20 @@ need:
 
 '''
 
+import numpy as np
+import scipy
+import pynapple as nap
+
+
+# helper function to get list of processed decoding result from em_res_l (multiple em fit results)
+def get_decode_res_l_from_em_res_l(em_res_l,load_res):
+    decode_res_l = []
+    for em_res in em_res_l:
+        log_posterior_final=em_res['log_posterior_final']
+        post_latent_marg = np.exp(scipy.special.logsumexp(log_posterior_final,axis=1))
+        post_dynamics_marg = np.exp(scipy.special.logsumexp(log_posterior_final,axis=2))
+        
+        decode_res_one = {'posterior_latent_marg':nap.TsdFrame(d=post_latent_marg,t=load_res['t_l']),'posterior_dynamics_marg':nap.TsdFrame(d=post_dynamics_marg,t=load_res['t_l'])}
+        
+        decode_res_l.append(decode_res_one)
+    return decode_res_l
