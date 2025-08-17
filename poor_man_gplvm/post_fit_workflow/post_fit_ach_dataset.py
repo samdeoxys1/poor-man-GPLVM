fa38362c69d013df75e20c1e4efec0c233414071
Sam Zheng <samzhenguchi@gmail.com>
2025-08-17 12:55:15 -0400

    load_data_and_fit_res

--- poor_man_gplvm/post_fit_workflow/post_fit_ach_dataset.py
+++ poor_man_gplvm/post_fit_workflow/post_fit_ach_dataset.py
@@ -3,25 +3,27 @@ For Yiyao Zhang's ACH dataset
 
 need:
 - transition events
-    - continuous and fragmented chunks
+    - detecting ACh bouts
         - further subdivide by sleep stage
-    - previously tried detecting ACh bouts; however, it can be more or less noisy depending on the session, making detection criteria sensitive to sessions; thus 
-    it's easier to detect the model-derived transition
+    - continuous and fragmented chunks (deprecated)
 - feature
-    - ach, pop fr, consec pv difference, 
+    - (ach), pop fr, consec pv difference, p_jump/continuous
 - peri transition feature average and shuffle
 - tuning
     - jump probability vs sleep stage
 - representational analysis 
     - (here the observation is the latent during continuous states tend to be similar within a NREM interval, but can be different across NREM intervals)
-    - mean latent posterior 
-- 
-
+    - feature: mean_latent_posterior, pv
+    - unit of comparison: ach bouts, ripples
+    - method: regression, dist ~ time + C(same nrem interval)
+        - null: shuffle the nrem interval label, so that if the two ach bouts used to be in the same nrem interval, they might not be after shuffle. But keep the time label correct?
+            - can do one for time too
 '''
 
 import numpy as np
 import scipy
 import pynapple as nap
+import dill
 
 
 # helper function to get list of processed decoding result from em_res_l (multiple em fit results)
@@ -37,3 +39,18 @@ def get_decode_res_l_from_em_res_l(em_res_l,t_l=None):
         
         decode_res_l.append(decode_res_one)
     return decode_res_l
+
+def load_data_and_fit_res(data_path,fit_res_path):
+    data_load_res = dill.load(open(data_path,'rb'))
+    fit_res_load_res = dill.load(open(fit_res_path,'rb'))
+    em_res_l = fit_res_load_res['em_res_l']
+    t_l = data_load_res['t_l']
+    decode_res_l = get_decode_res_l_from_em_res_l(em_res_l,t_l)
+    model_eval_result = fit_res_load_res['metric_eval_result']
+    model_index=model_eval_result['metric_overall']['best_index']
+    model_fit = fit_res_load_res['model_fit_l'][model_index]
+    decode_res = decode_res_l[model_index]
+
+    prep_res = {**data_load_res,**decode_res,'model_fit':model_fit}
+
+    return prep_res
