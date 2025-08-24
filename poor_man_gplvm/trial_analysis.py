06ba663d120d0c5f7fc75822c8eae9e11901f5df
Sam Zheng <samzhenguchi@gmail.com>
2025-08-24 16:35:33 -0400

    get_event_phase_in_trial

---
+++ poor_man_gplvm/trial_analysis.py
@@ -0,0 +1,15 @@
+'''
+helper function for analyzing trial-by-trial data
+'''
+
+def get_event_phase_in_trial(trials,event_key_l=[],span_key=['start','end']):
+    '''
+    get the phase of the event in the trial
+    span_key define the start and end of the epoch we look at, usually 'start' and 'end' of the trial
+    '''
+    event_phase_per_trial_per_event = {}
+    for event_key in event_key_l:
+        event_phase_per_trial_per_event[event_key] = (trials[event_key] - trials[span_key[0]]) / (trials[span_key[1]] - trials[span_key[0]])
+    event_phase_per_trial_per_event = pd.DataFrame(event_phase_per_trial_per_event)
+    return event_phase_per_trial_per_event
+
