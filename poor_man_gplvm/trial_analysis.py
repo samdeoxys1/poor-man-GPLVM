81d05dfc897bca361d80a8d2bac472b431ef5ef9
Sam Zheng <samzhenguchi@gmail.com>
2025-08-24 16:53:52 -0400

    fix

--- poor_man_gplvm/trial_analysis.py
+++ poor_man_gplvm/trial_analysis.py
@@ -16,13 +16,15 @@ def get_event_phase_in_trial(trials,event_key_l=[],span_key=['start','end']):
     event_phase_per_trial_per_event = pd.DataFrame(event_phase_per_trial_per_event)
     return event_phase_per_trial_per_event
 
-def plot_event_range(event_phase_df,xs,event_key_l=None,ax=None,quantile_range=[0.25,0.75],alpha=0.5,palette='Set1'):
+def plot_event_range(event_phase_df,xs=None,event_key_l=None,ax=None,quantile_range=[0.25,0.75],alpha=0.5,palette='Set1'):
     '''
-
+    event_phase_df: n_trials x n_events
+    plot shaded region for the event range, with the quantile range
     '''
     if event_key_l is None:
         event_key_l = event_phase_df.columns.tolist()
-    
+    if xs is None: # if not provided, assume plot has been normalized to [0,1]
+        xs=(0,1)
     palette = sns.color_palette(palette, len(event_key_l))
     for i,event_key in enumerate(event_key_l):
         left_,right_ = event_phase_df[event_key].quantile(quantile_range[0]),event_phase_df[event_key].quantile(quantile_range[1])
