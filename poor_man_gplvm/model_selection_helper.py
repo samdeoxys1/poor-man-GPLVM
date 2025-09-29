7c75053a56cd023dceffa7c1be984dac777be24c
Sam Zheng <samzhenguchi@gmail.com>
2025-09-29 15:58:28 -0400

    fix

--- poor_man_gplvm/model_selection_helper.py
+++ poor_man_gplvm/model_selection_helper.py
@@ -334,7 +334,8 @@ def get_jump_consensus_shuffle(jump_p, jump_p_all_chain, chain_index, n_shuffle=
     # For each shuffle, generate shift amounts for all chains
     def generate_shifts_for_shuffle(shuffle_key):
         chain_keys = jr.split(shuffle_key, n_other_chains)
-        return jr.randint(chain_keys, shape=(n_other_chains,), minval=0, maxval=n_time)
+        # Use vmap to apply randint to each chain key
+        return jax.vmap(lambda k: jr.randint(k, shape=(), minval=0, maxval=n_time))(chain_keys)
     
     # Vectorize over all shuffles: shape (n_shuffle, n_other_chains)
     shift_amounts = jax.vmap(generate_shifts_for_shuffle)(shuffle_keys)
