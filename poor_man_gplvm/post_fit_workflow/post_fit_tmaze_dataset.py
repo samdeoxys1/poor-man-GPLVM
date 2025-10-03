8e45f759f405da940c7c1010c1a74046a618eb58
Sam Zheng <samzhenguchi@gmail.com>
2025-10-03 13:10:23 -0400

    fix

--- poor_man_gplvm/post_fit_workflow/post_fit_tmaze_dataset.py
+++ poor_man_gplvm/post_fit_workflow/post_fit_tmaze_dataset.py
@@ -91,6 +91,7 @@ def classify_latent(map_latent,position_tsdf,speed_tsd,tmaze_xy_sampled_all,spee
             is_immobility_all_latent[latent_i] = True
             is_off_maze_all_latent[latent_i] = False
         if len(latent_run_index)>0:
+            xy_l = position_tsdf[latent_run_index]['x','y'].d
             dist_to_maze=get_dist_to_maze(xy_l,tmaze_xy_sampled_all)
             n_off_maze_time = (dist_to_maze >dist_to_maze_thresh).sum()
             if n_off_maze_time > min_off_maze_time:
