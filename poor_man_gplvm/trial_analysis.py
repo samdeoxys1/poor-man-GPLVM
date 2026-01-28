c7da476f2570a4b38d99a887e883a48e163a6b68
Sam Zheng <samzhenguchi@gmail.com>
2026-01-28 16:47:25 -0500

    bin_spike_train_to_trial_based

--- poor_man_gplvm/trial_analysis.py
+++ poor_man_gplvm/trial_analysis.py
@@ -32,3 +32,39 @@ def plot_event_range(event_phase_df,xs=None,event_key_l=None,ax=None,quantile_ra
         right=(xs[-1]-xs[0]) * right_ + xs[0]
         ax.axvspan(left,right,color=palette[i],alpha=alpha)
     return ax
+
+# turn continuous-time data into trial / event-based data
+def bin_spike_train_to_trial_based(spike_train,trial_intervals,binsize=0.02):
+    '''
+    spike_train: nap.TsGroup
+    trial_intervals: nap.IntervalSet; can be any event, e.g. population burst events
+
+    return:
+    spike_mat_padded: n_trial x n_bin x n_neuron; padded with 0 
+    mask: n_trial x n_bin x 1; True for valid bins
+    event_index_per_bin: n_bin; index of the event for each bin
+    n_bin_per_trial: n_trial; number of bins per trial
+    '''
+
+    # concatenated spike matrix
+    spike_mat = spike_train.count(binsize=binsize,ep=trial_intervals)
+
+    # event index for each bin
+    event_index_per_bin = trial_intervals.in_interval(nap.Ts(spike_mat.t))
+
+    # padded tensor
+    spike_mat_padded = nap.build_tensor(spike_mat,ep=trial_intervals,bin_size=binsize) # n_neuron x n_trial x n_bin
+    spike_mat_padded = spike_mat_padded.swapaxes(0,1).swapaxes(1,2) # n_trial x n_bin x n_neuron
+
+    # mask
+    mask_full = np.logical_not(np.isnan(spike_mat_padded))
+    mask = mask_full[:,:,[0]] # n_trial x n_bin x 1
+
+    # n_bin of each trial
+    n_bin_per_trial = np.squeeze(mask_full.sum(axis=1))
+
+    # repad with 0 to avoid nan
+    spike_mat_padded[mask_full] = 0
+
+    bin_spk_res = {'spike_mat_padded': spike_mat_padded, 'mask': mask, 'event_index_per_bin': event_index_per_bin, 'n_bin_per_trial': n_bin_per_trial}
+    return bin_spk_res
