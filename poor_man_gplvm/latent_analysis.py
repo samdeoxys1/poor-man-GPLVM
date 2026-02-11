3d0471e2fd2f0ea7f687493015a060f4be70ac36
Sam Zheng <samzhenguchi@gmail.com>
2026-02-11 11:08:01 -0500

    fix

--- poor_man_gplvm/latent_analysis.py
+++ poor_man_gplvm/latent_analysis.py
@@ -15,10 +15,10 @@ def compute_mean_latent_per_behavior(p_latent_marg_beh, ep_d):
 
 def classify_latents_by_behavior(p_latent_marg_beh,ep_d, thresh=0.5):
     mean_latent_per_beh = compute_mean_latent_per_behavior(p_latent_marg_beh, ep_d)
-    score_df = mean_latent_per_beh / mean_latent_per_beh.sum(axis=1).values[:, None]
+    latent_score_df = mean_latent_per_beh / mean_latent_per_beh.sum(axis=1).values[:, None]
     latent_type_ma_d = {}
-    for col in score_df.columns:
-        latent_type_ma_d[col] = score_df[col] >= thresh
-    latent_type_ma_d['unclassified'] = (score_df <= thresh).all(axis=1)
+    for col in latent_score_df.columns:
+        latent_type_ma_d[col] = latent_score_df[col] >= thresh
+    latent_type_ma_d['unclassified'] = (latent_score_df <= thresh).all(axis=1)
     latent_type_df = pd.concat(latent_type_ma_d, axis=1)
-    return latent_type_df,score_df
+    return latent_type_df,latent_score_df
